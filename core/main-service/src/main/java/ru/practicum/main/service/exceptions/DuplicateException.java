78772aba7ada2f241a2e68a92df335692a65fecb
Boris <Zelyanin>
2025-08-10 13:55:32 +0300

    create a new function

--- core/main-service/src/main/java/ru/practicum/main/service/exceptions/DuplicateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.main.service.exceptions;
-
-public class DuplicateException extends RuntimeException {
-    public DuplicateException(String message) {
-        super(message);
-    }
-}
